cc00aa84a249cb6bd7f2bec1da816ca4b1d98ff2
Namaumbo <daelo.namaumbo@gmail.com>
2025-02-16 20:02:37 +0200

    rf

--- config/database.ts
+++ config/database.ts
@@ -42,11 +42,14 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
+      port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),
+      ssl: env.bool('DATABASE_SSL', false) && {
+        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+      },
     },
+    debug: false,
   },
 });
